577d97e7df1c47c30814a37c217374fc2c21a26a
Serhii <avokaqq1992@gmail.com>
2024-08-08 02:28:34 +0200

    com

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+
+        private WebDriver driver;
+
+        @BeforeTest
+        void setupDriver(){
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+            driver = new ChromeDriver();
+            driver.get("https://testpages.eviltester.com/styled/calculator");
+        }
+
+        @Test
+        public void myTest(){
+
+            WebElement firstNumberElement = driver.findElement(By.id("number1"));
+            firstNumberElement.sendKeys("2");
+
+            WebElement secondNumberElement = driver.findElement(By.name("number2"));
+            secondNumberElement.sendKeys("7");
+
+            WebElement submitButton = driver.findElement(By.xpath("//input[@id=\"calculate\"]"));
+            submitButton.click();
+
+            WebElement answer = driver.findElement(By.id("answer"));
+
+            Assert.assertEquals(answer.getText(), "9", "The result is incorrect, should be 9");
+        }
+
+        @AfterTest
+        void closeDriver(){
+            driver.quit();
+        }
+
+
+
+
+
+}
